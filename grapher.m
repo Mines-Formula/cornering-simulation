379dc1572b97442f3dd15e42a200ccc3883980d8
PMurphyB <pmurphyguitar@gmail.com>
2025-09-27 10:39:00 -0600

    Initial Graphing Tool

--- grapher.m
+++ grapher.m
@@ -1,2 +1,15 @@
 clc, clearvars, clear all
+inFile = "R20_sorted.csv";
 
+newTable = readtable(inFile);
+
+Y = newTable.LateralForce;
+X = newTable.SlipAngle;
+
+figure;
+plot(X, Y, 'b-', 'LineWidth', 0.1);
+xlabel('Slip Angle (deg)'); 
+ylabel('Cornering Force (kN)'); 
+xlim(-10, 10);
+ylim(-5, 5);
+grid on;
