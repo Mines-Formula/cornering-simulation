299292b94534db7f993dda7f565c5f8a30de9f95
PMurphyB <pmurphyguitar@gmail.com>
2025-09-27 09:58:29 -0600

    Fixed Title

--- sorter.m
+++ sorter.m
@@ -4,7 +4,7 @@ dataFolder = '/Users/Blanchards1/Documents/FormulaSim/documentation/data/runData
 inFile  = fullfile(dataFolder, "R20.csv");
 
 newTable = readtable(inFile);
-genericSort(newTable, {'TirePressure', 'RoadSpeed'}, "R20_ordered_tire_pressureve_and_speed.csv");
+genericSort(newTable, {'TirePressure', 'RoadSpeed'}, "R20_ordered_tire_pressure_and_speed.csv");
 
 function [newTable] = genericSort(startTable, sortVars, outFile)
     newTable = sortrows(startTable, sortVars);
