f4b6b1521f3652bd322d09645e147f74ff30dd24
PMurphyB <pmurphyguitar@gmail.com>
2025-09-27 11:32:01 -0600

    Update grapher.m

--- grapher.m
+++ grapher.m
@@ -3,13 +3,4 @@ inFile = "R20_sorted.csv";
 
 newTable = readtable(inFile);
 
-Y = newTable.LateralForce;
-X = newTable.SlipAngle;
-
-figure;
-plot(X, Y, 'b-', 'LineWidth', 0.1);
-xlabel('Slip Angle (deg)'); 
-ylabel('Cornering Force (kN)'); 
-xlim(-10, 10);
-ylim(-5, 5);
-grid on;
+uniqueLoads = unique(newTable.NormalForce);
