ee51d8f8035ef55ea0c60552c1832a671b393ae1
PMurphyB <pmurphyguitar@gmail.com>
2025-09-27 13:57:55 -0600

    Adding Loops for each load

--- grapher.m
+++ grapher.m
@@ -2,25 +2,25 @@ clc, clearvars, clear all
 inFile = "R20_sorted.csv";
 
 newTable = readtable(inFile);
-
-uniqueLoads = unique(newTable.NormalForce);
 slipAngle = newTable.SlipAngle;
 corneringForce = newTable.LateralForce;
+normalForce = newTable.NormalForce;
 
-order = 1;
+uniqueLoads = unique(newTable.NormalForce);
+
+
+order = 2;
 cutoff = 0.05;
 [b, a] = butter(order, cutoff, 'low');
-corneringForceFilter = filtfilt(b, a, corneringForce);
 
 figure('Color', [1 1 1]);
-scatter(slipAngle, corneringForce, 1, 'b', 'filled');
 hold on;
 grid on;
+colors = lines(length(uniqueLoads));
 
-
-plot(slipAngle, corneringForceFilter, 'r-', 'LineWidth', 2);
-xlabel('Slip Angle [deg]');
-ylabel('Cornering Force [N]');
-title('Cornering Force vs Slip Angle (Filtered)');
-legend('Raw Data','Butterworth Filtered','Location','best');
-grid on
+for i = 1:length(uniqueLoads)
+    thisLoad = uniqueLoads(i);
+    idx = (normalForce == thisLoad);
+    sa = slipAngle(idx);
+    cf = corneringForce(idx);
+end
