e0fb565f8cf2d756bd9e57451d1dfd0dcf1c30b4
PMurphyB <pmurphyguitar@gmail.com>
2025-09-29 10:21:50 -0600

    Added butterworth filter

--- grapher.m
+++ grapher.m
@@ -9,6 +9,10 @@ normalForce = newTable.NormalForce;
 loadBins = round(normalForce / 50) * 50;
 uniqueLoads = unique(loadBins);
 
+order = 2;
+cutoff = 0.05;
+[b, a] = butter(order, cutoff, 'low');
+
 colors = lines(length(uniqueLoads));
 
 figure('Color', [1,1,1]);
@@ -18,6 +22,14 @@ grid on;
 for i = 1:length(uniqueLoads)
     thisLoad = uniqueLoads(i);
     idx = (loadBins == thisLoad);
+
+    sa = slipAngle(idx);
+    cf = corneringForce(idx);
+
+    [saSorted, sortIdx] = sort(sa);
+    cfSorted = cf(sortIdx);
+
+    cfFilt = filtfilt(b, a, cfSorted);
 
     scatter(slipAngle(idx), corneringForce(idx), 1, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.4);
 end
