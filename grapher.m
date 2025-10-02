25f9ea4471141fc7ae1aadc2c57b8b00d636c8f2
PMurphyB <pmurphyguitar@gmail.com>
2025-10-02 13:47:57 -0600

    Removed step for now, might not need it

--- grapher.m
+++ grapher.m
@@ -76,10 +76,10 @@ for i = 1:length(uniqueLoads)
     smoothCorneringForce = filtfilt(b, a, binnedCorneringForce);
 
     % Added step
-    step = 20;
-    saPlot = saSorted(1:step:end);
-    cfPlot = cfSorted(1:step:end);
-    scatter(saSorted, cfSorted, 1, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.2);
+    %%step = 20;
+    %saPlot = saSorted(1:step:end);
+    %cfPlot = cfSorted(1:step:end);
+    %scatter(saSorted, cfSorted, 1, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.2);
 
     plot(saSorted, cfFilt, 'LineWidth', 2, 'Color', colors(i,:));
 end
