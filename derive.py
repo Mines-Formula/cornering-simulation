6496907b0b32fd62e1c5b17d6bf72983355113ab
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2025-10-10 14:04:14 -0600

    Added steps 6 and 7

--- derive.py
+++ derive.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 from scipy.special import expit
 from scipy.optimize import curve_fit
+from scipy.differentiate import derivative
 import sympy as sp
 
 df = pd.read_csv("C:/Users/ajsau/Documents/formula/corneringSim/cornering-simulation/LCO_ordered_normal_force.csv")
@@ -69,17 +70,35 @@ Cy = pcy1
 
 
 #step 5 pey1
-#Dy is from step 2 not from step 4?
-#Cy is from step 4
+#Dy is probably from step 2?
+#Cy and pcy1 is from step 4
 #xm is also from step 2 - peak slip angle in radians
 #where does slope of fz0 curve come from? - sFz0
-#using example data so I know it will work
+#**example data**
 pcy1 = 1.5
 sFz0 = -73456
 Cy = pcy1
 Dy = -3050
 xm = 0.14
+#**end example data**
 By = sFz0 / (Cy * Dy)
 Bxm = By * xm
 pey1 = (Bxm - math.tan(math.pi/(2*pcy1)))/(Bxm - math.atan(Bxm))
 print(f"pey1 = {pey1}")
+
+
+#step 6
+#xoffset - get x intercept of step 2 graph
+#**example data**
+xoffset = 0.5 
+#**end example data**
+phy1 = xoffset * (math.pi/180)
+print(f"Horizontal shift phy1: {phy1}")
+
+#step 7
+#yoffset = get y intercept of step 2 graph
+#**example data**
+yoffset = 40
+#**end example data**
+pvy1 = yoffset/fz0
+print(f"Value of pvy1: {pvy1}")
