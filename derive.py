e5db7ea94a4418432be6570785c644b872b9b4a2
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2025-10-10 13:13:38 -0600

    added some debugging to the exponential function

--- derive.py
+++ derive.py
@@ -4,6 +4,7 @@ import math
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 from scipy.optimize import curve_fit
+from scipy.differentiate import derivative
 import sympy as sp
 
 df = pd.read_csv("C:/Users/ajsau/Documents/formula/corneringSim/cornering-simulation/LCO_ordered_normal_force.csv")
@@ -37,19 +38,27 @@ def exponential(x, r, k, y0):
 popt, pcov = curve_fit(exponential, x, y, p0=(10,1,1))
 print(popt)
 x_wahoo = np.linspace(0,9,9000)
+
+x_symbol, r_symbol, k_symbol, y_symbol, = sp.symbols('x r k y')
+eqn_exponential = r_symbol * sp.exp(-k_symbol * x_symbol) - y_symbol
+exponential_deriv = sp.diff(eqn_exponential, x_symbol)
+print(f"Rate of change at 7.400kN: {exponential_deriv.subs({r_symbol: popt[0], k_symbol: popt[1], y_symbol: popt[2], x_symbol: 7.4}).evalf()}")
+print(f"Rate of change at 1.962kN: {exponential_deriv.subs({r_symbol: popt[0], k_symbol: popt[1], y_symbol: popt[2], x_symbol: 1.962}).evalf()}")
+print("Value at 7.40kN: " + str(exponential(7.4, *popt)))
 plt.plot(x_wahoo, exponential(x_wahoo, *popt), 'r-')
 plt.scatter(x, y)
-plt.show()
+plt.xlim(0, 20)
+#plt.show()
 
 #former polynomial
-'''
 values = np.polyfit(x, y, 2)
 eqn = np.poly1d(values)
 print(type(eqn))
+print(eqn)
 
 #take the derivative
 eqn_deriv = eqn.deriv()
-#print(f"Derivative: {eqn_deriv}")
+print(f"Derivative: {eqn_deriv}")
 #find the roots based on the coeff
 #root = max value of stiffness (still of type list)
 roots = np.roots(eqn_deriv.coeffs)
@@ -62,10 +71,8 @@ pxy2 = (max_vertical_load_poly/fz0)
 print(pxy2)
 
 x_wahoo = list(range(0,9000))
-plt.scatter(x, y)
 plt.plot(x_wahoo, eqn(x_wahoo), color="green")
 plt.plot(x_wahoo, eqn_deriv(x_wahoo), color="red")
 plt.xlim(0, 9000)
 plt.ylim(-90, 10)
 plt.show()
-'''
