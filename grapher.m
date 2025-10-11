ad6699fc2d7f850890c8b1cbc2c2fdd43980667c
PMurphyB <pmurphyguitar@gmail.com>
2025-10-11 00:07:38 -0600

    Fixed Misspellings and moved stuff around

--- grapher.m
+++ grapher.m
@@ -31,6 +31,7 @@ grid on;
 
 for i = 1:length(uniqueLoads)
 
+    brightColor = min(colors(i,:) * 1.5, 1.0);
     thisLoad = uniqueLoads(i);
     idx = (loadBins == thisLoad);
     curSlipAngle = slipAngleRaw(idx);
@@ -104,11 +105,11 @@ for i = 1:length(uniqueLoads)
 
 
     % pacejka function for best fit
-    pacejkaFunction = @(params, alphs) params(3) .* sin(params(2) .* sin(params(2) .* atan(params(1) * alpha - params(4) * params(1) * alpha - atan(params(1) * alpha))));
+    pacejkaFunction = @(params, alpha) params(3) .* sin(params(2) .* sin(params(2) .* atan(params(1) * alpha - params(4) * params(1) * alpha - atan(params(1) * alpha))));
 
     initialGuess = [10, 1.3, Dy, 0.97];
 
-    opts = opimoptions('lsqcurvefit', 'Display', 'off');
+    opts = optimoptions('lsqcurvefit', 'Display', 'off');
     params = lsqcurvefit(pacejkaFunction, initialGuess, binnedSlipAngle * pi / 180, loessForce, [], [], opts); 
 
     B = params(1);
@@ -116,15 +117,15 @@ for i = 1:length(uniqueLoads)
     D = params(3);
     E = params(4);
 
-    fprintf('Load = %d N --> B=%.3f, C=%.3f, D=%.2f, E=%.3\n', thisLoad, B, C, D, E);
+    fprintf('Load = %d N --> B=%.3f, C=%.3f, D=%.2f, E=%.3f\n', thisLoad, B, C, D, E);
 
-    alphaFine = linespace(min(binnedSlipAngle), max(binnedSlipAngle), 200);
+    alphaFine = linspace(min(binnedSlipAngle), max(binnedSlipAngle), 200);
     FyFit = pacejkaFunction(params, alphaFine * pi / 180);
 
     plot(alphaFine, FyFit, '-', 'Color', brightColor, 'LineWidth', 2.5, 'DisplayName', sprintf('%d N (Pacejka Fit)', thisLoad));
 
-    if ~exit('fitResults', 'var')
-        firResults = table(thisLoad, B, C, D, E);
+    if ~exist('fitResults', 'var')
+        fitResults = table(thisLoad, B, C, D, E);
     else
         fitResults = [fitResults; table(thisLoad, B, C, D, E)];
     end
@@ -137,10 +138,9 @@ for i = 1:length(uniqueLoads)
     end
 
     % This is the previous plot just for comparison
-    plot(binnedSlipAngle, corneringForceSmooth, '-', 'LineWidth', 2, 'Color', colors(i,:), 'DisplayName', sprintf('%d kg (ORIGINAL)', abs(thisLoad)));
+    %plot(binnedSlipAngle, corneringForceSmooth, '-', 'LineWidth', 2, 'Color', colors(i,:), 'DisplayName', sprintf('%d kg (ORIGINAL)', abs(thisLoad)));
 
     % This is the new LOESS based plot
-    brightColor = min(colors(i,:) * 1.5, 1.0);
     plot(binnedSlipAngle, loessForce, '--', 'Color', brightColor, 'LineWidth', 2, 'DisplayName', sprintf('%d kg (LOESS)', abs(thisLoad)));
     
 end
